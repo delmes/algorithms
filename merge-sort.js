bfc74bab54361bc33c44c1d10faf46f82ac095d2
deltawang <deltawang@tencent.com>
2018-05-21 13:17:58 +0800

    add some commeent

--- merge-sort.js
+++ merge-sort.js
@@ -1,11 +1,11 @@
-var arr = [21,1,33,2,42343,2,1,3112,78];
+// 归并排序先不断切割划分，然后两两合并
+var arr = [21,1,33,2,4243,2,1,3112,78];
 var sort = function(arr, start, end) {
     var middle = Math.floor((start + end) / 2);
     if (start < end) {
         sort(arr, start, middle);
         sort(arr, middle + 1, end);
         merge(arr, start, middle, end);
-      
     }
     return arr;
 };
@@ -26,6 +26,7 @@ var merge = function(arr, start, middle, end) {
             k++;
         }
     }
+    // 此处有可能其中一个比另外一个长，所以就把剩下的拼接上去
     while(left <= middle) {
         tempArr[k] = arr[left];
         k++;
@@ -36,7 +37,7 @@ var merge = function(arr, start, middle, end) {
         k++;
         right++;
     }
-    
+    // 拼接完成的新数组tempArr放回原处，保证start -> end这一小段是有序的
     for (var j = 0; j < tempArr.length; j++) {
         arr[start + j] = tempArr[j];
     }
